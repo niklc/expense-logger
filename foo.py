06efa54a8910751241d86fc22bb5a0de15fcb817
Nikolas Cinis <nikolas.cinis@gmail.com>
2020-11-14 21:42:43 +0200

    flask return html form and submit end-point

--- foo.py
+++ foo.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def form_log_expense():
+    return app.send_static_file('form_log_expense.html')
+
+
+@app.route('/post-expense', methods=['POST'])
+def post_expense():
+    return 'posted'
